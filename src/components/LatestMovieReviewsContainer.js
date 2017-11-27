9e28cd89e70f69b0e7bbe5e9179bf2df31739162
gene-yoo <gene.y.yoo@gmail.com>
2017-11-26 23:02:18 -0500

    Done.

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -1,8 +1,38 @@
-import React, { Component } from 'react';
-import 'isomorphic-fetch';
+import React from "react";
+import MovieReviews from "./MovieReviews";
 
-const NYT_API_KEY = 'f98593a095b44546bf4073744b540da0';
-const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
-            + `api-key=${NYT_API_KEY}`;
+import "isomorphic-fetch";
+
+const NYT_API_KEY = "";
+const URL =
+  "https://api.nytimes.com/svc/movies/v2/reviews/all.json?" +
+  `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reviews: []
+    };
+  }
+
+  componentWillMount() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(reviews => this.setState({ reviews }));
+  }
+
+  render() {
+    return (
+      <MovieReviews
+        className="latest-movie-reviews"
+        reviews={this.state.reviews}
+      />
+    );
+  }
+}
+
+export default LatestMovieReviewsContainer;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,1 +1,36 @@
 // Code SearchableMovieReviewsContainer Here
+import React from "react";
+import MovieReviews from "./MovieReviews";
+
+import "isomorphic-fetch";
+
+const NYT_API_KEY = "";
+const URL = "https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=";
+
+class SearchableMovieReviewsContainer extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchTerm: "",
+      reviews: []
+    };
+  }
+
+  componentWillMount() {
+    fetch(URL + this.state.searchTerm + `api-key=${NYT_API_KEY}`)
+      .then(res => res.json())
+      .then(reviews => this.setState({ reviews }));
+  }
+
+  render() {
+    return (
+      <MovieReviews
+        className="searchable-movie-reviews"
+        reviews={this.state.reviews}
+      />
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
